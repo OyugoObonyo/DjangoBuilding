791b67cb2a99d9b95b73187037fe5eacf6feb3e1
OyugoObonyo <nyikal1999@gmail.com>
2022-04-15 10:13:02 +0300

    Add buildings model

--- buildings/models.py
+++ buildings/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Building(models.Model):
+    """
+    
+    A class representing the building table in the db
+    """
+
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    owner = models.CharField(max_length=50)
+    units_count = models.IntegerField
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Building id:{self.id}, Building name: {self.name}"

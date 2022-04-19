73cddb9e9d1dd90f595d43a6cf2f3aa755ff917d
OyugoObonyo <nyikal1999@gmail.com>
2022-04-19 12:57:35 +0300

    create view for adding buildings

--- buildings/views.py
+++ buildings/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import render, redirect
+from .forms import AddBuildingForm
 
-# Create your views here.
+
+def add_building(request):
+    """
+
+    add_building adds a building to the db
+    """
+    form = AddBuildingForm()
+    if request.method == 'POST':
+        form = AddBuildingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'buildings/create_building_form.html', context)

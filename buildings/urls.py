848fefc78e15f76d262ef93adb9e3b722c76d08e
OyugoObonyo <nyikal1999@gmail.com>
2022-04-19 16:26:50 +0300

    create url for updating buildings

--- buildings/urls.py
+++ buildings/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('add-building', views.add_building, name="add-building")
+    path('add-building/', views.add_building, name="add-building"),
+    path('update-building/<int:id>/', views.update_building, name="update-building")
 ]
